ec46c1cc4bcde367bde56305cf6c8b22a40b87ed
Ali Mohamed <aliabdelazeem20@gmail.com>
2024-03-15 11:33:12 +0200

    update tarmiz api with fetch approach

--- 1-tarmiz-API-1/main.js
+++ 1-tarmiz-API-1/main.js
@@ -1,54 +1,75 @@
+
+
 function getposts(id) {
-  const request = new XMLHttpRequest();
 
-  request.open("GET", `https://jsonplaceholder.typicode.com/posts?userId=`+id);
-  request.send();
-  request.responseType = "json";
 
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 300) {
-      console.log("Commits:", request.response);
-      let posts = request.response;
 
-      // fill html file with posts
-      const divPosts = document.querySelector(".posts");
-      divPosts.innerHTML = "";
-      for (let post of posts) {
-        const divPosts = document.querySelector(".posts");
-        divPosts.innerHTML += `
+// Define the URL you want to fetch data from
+const url2 =  `https://jsonplaceholder.typicode.com/posts?userId=`+id;
 
-      <div class="post">
-          <p class="title">${post.title}</p>
-          <p class="content">
-            ${post.body}
-          </p>
-      </div>  
+// Make a fetch request to the URL
+fetch(url2)
+.then(response => {
+  // Check if the response is successful
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  // Parse the JSON from the response
+  
+  return response.json();
+})
+.then(posts => {
 
+
+  const divPosts = document.querySelector(".posts");
+  divPosts.innerHTML = "";
+  for (let post of posts) {
+    divPosts.innerHTML += `
+    <div class="post">
+        <p class="title">${post.title}</p>
+        <p class="content">
+          ${post.body}
+        </p>
+    </div>  
 `;
-      }
-      // fill html file with posts
-    } else {
-      console.error("Error fetching commits:", request.statusText);
-    }
-  };
+  }
+
 }
-//###############
-//###############
+
+
+)
+.catch(error => {
+  // Handle any errors that occur during the fetch
+  console.error('There was a problem with the fetch operation:', error);
+});
+
+}
+
+
+  
+
 
 
 function getUsers() {
-  const request = new XMLHttpRequest();
 
-  request.open("GET", `https://jsonplaceholder.typicode.com/users`);
-  request.send();
-  request.responseType = "json";
+  return new Promise((resolve , reject)=> {
 
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 300) {
-      console.log("Commits:", request.response);
-      let users = request.response;
-
-      // fill html file with posts
+    // Define the URL you want to fetch data from
+    const url2 =  `https://jsonplaceholder.typicode.com/users`;
+    
+    // Make a fetch request to the URL
+    fetch(url2)
+    .then(response => {
+      // Check if the response is successful
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      // Parse the JSON from the response
+      return response.json();
+    })
+    .then(users => {
+    
+    
       const divUsers = document.querySelector(".users");
       divUsers.innerHTML = "";
       for (let user of users) {
@@ -62,16 +83,23 @@ function getUsers() {
 
 `;
       }
-      // fill html file with posts
-    } else {
-      console.error("Error fetching commits:", request.statusText);
+      resolve()
+    })
+    .catch(error => {
+      // Handle any errors that occur during the fetch
+      console.error('There was a problem with the fetch operation:', error);
+    });
+    
     }
-  };
-}
+    
+  
+    )
+  }
 
-
-getposts(1);
-getUsers()
+;
+getUsers().then(()=> {
+  getposts(1)
+})
 
 function runPosts(id) {
   getposts(id)
@@ -93,121 +121,9 @@ user.classList.add("selected")
 }
 
 
-// setTimeout(() => {
-//   console.log('Async operation 1 completed')
-//   setTimeout(() => {
-//     console.log('Async operation 2 completed')
-//     setTimeout(() => {
-//       console.log('Async operation 3 completed')
-      
-//     }, 2000);
-//   }, 2000);
-// }, 2000);
-
-
-// function asyncOperation1() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log('Async operation 1 completed');
-//       resolve('Result from operation 1');
-//     }, 1000);
-//   });
-// }
-
-// function asyncOperation2() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log('Async operation 2 completed');
-//       resolve('Result from operation 2');
-//     }, 1500);
-//   });
-// }
-
-// function asyncOperation3() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log('Async operation 3 completed');
-//       resolve('Result from operation 3');
-//     }, 2000);
-//   });
-// }
-
-// asyncOperation1()
-//   .then(result1 => {
-//     console.log(result1);
-//     return asyncOperation2();
-//   })
-//   .then(result2 => {
-//     console.log(result2);
-//     return asyncOperation3();
-//   })
-//   .then(result3 => {
-//     console.log(result3);
-//     console.log('All operations completed');
-//   })
-//   .catch(error => {
-//     console.error('An error occurred:', error);
-//   });
 
 
 
 
-
-
-
-  // Define the URL you want to fetch data from
-const url2 = `https:/z/jsonplaceholder.typicode.com/users`;
-
-// Make a fetch request to the URL
-fetch(url2)
-.then(response => {
-  // Check if the response is successful
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  // Parse the JSON from the response
-  return response.json();
-})
-.then(data => {
-  // Do something with the data
-  console.log(data);
-})
-.catch(error => {
-  // Handle any errors that occur during the fetch
-  console.error('There was a problem with the fetch operation:', error);
-});
-
-
-
-
-
-    // Define the URL you want to fetch data from
-const url = `https://jsonplaceholder.typicode.com/users`;
-
-// Make a GET request using Axios
-axios.get(url)
-  .then(response => {
-    // Handle the successful response
-    console.log(response);
-    console.log(response.data);
-  })
-  .then((ss)=> {
-    console.log(ss)
-  })
-
-
-
-  .catch(error => {
-    // Handle any errors that occur during the request
-    console.error('There was an error!', error);
-  });
-
-
-
-  let students = [85 , 90 , 60, 12] 
-
-  if ( students >= 90 ) {
-
-  }
 
 
