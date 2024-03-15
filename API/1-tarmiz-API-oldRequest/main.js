2c07bfb4ac986c6f2a0698270ed4f6011014a156
Ali Mohamed <aliabdelazeem20@gmail.com>
2024-03-15 14:52:08 +0200

    adding three type apis

---
+++ API/1-tarmiz-API-oldRequest/main.js
@@ -0,0 +1,96 @@
+function getposts(id) {
+  const request = new XMLHttpRequest();
+
+  request.open("GET", `https://jsonplaceholder.typicode.com/posts?userId=`+id);
+  request.send();
+  request.responseType = "json";
+
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 300) {
+      console.log("Commits:", request.response);
+      let posts = request.response;
+
+      // fill html file with posts
+      const divPosts = document.querySelector(".posts");
+      divPosts.innerHTML = "";
+      for (let post of posts) {
+        const divPosts = document.querySelector(".posts");
+        divPosts.innerHTML += `
+
+      <div class="post">
+          <p class="title">${post.title}</p>
+          <p class="content">
+            ${post.body}
+          </p>
+      </div>  
+
+`;
+      }
+      // fill html file with posts
+    } else {
+      console.error("Error fetching commits:", request.statusText);
+    }
+  };
+}
+//###############
+//###############
+
+
+function getUsers() {
+  const request = new XMLHttpRequest();
+
+  request.open("GET", `https://jsonplaceholder.typicode.com/users`);
+  request.send();
+  request.responseType = "json";
+
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 300) {
+      console.log("Commits:", request.response);
+      let users = request.response;
+
+      // fill html file with posts
+      const divUsers = document.querySelector(".users");
+      divUsers.innerHTML = "";
+      for (let user of users) {
+
+        divUsers.innerHTML += `
+
+        <div class="user" onclick="runPosts(${user.id})">
+        <p class="name">${user.name}</p>
+        <p class="email">${user.email}</p>
+      </div> 
+
+`;
+      }
+      // fill html file with posts
+    } else {
+      console.error("Error fetching commits:", request.statusText);
+    }
+  };
+}
+
+
+getposts(1);
+getUsers()
+
+function runPosts(id) {
+  getposts(id)
+
+  const allUsers = document.getElementsByClassName("user")
+
+  for (let user of allUsers){
+    user.addEventListener("click" ,()=>{
+console.log("dddddd")
+for(let el of allUsers) {
+  el.classList.remove("selected")
+}
+user.classList.add("selected")
+
+    })
+
+  }
+
+}
+
+
+
